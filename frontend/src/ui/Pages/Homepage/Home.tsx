a51a7f36f89894d85f3628697b3c08bbe05bbeb1
DorindaAbeyta <129131324+DorindaAbeyta@users.noreply.github.com>
2023-06-14 11:04:33 -0600

    changed home to include the navigation and took away fillers

--- frontend/src/ui/Pages/Homepage/Home.tsx
+++ frontend/src/ui/Pages/Homepage/Home.tsx
@@ -1,6 +1,5 @@
-import {DreamPost} from "../Posts/Posts.tsx";
-import {Col, Container, Navbar, Row} from "react-bootstrap";
 import {PostFormModal} from "../../PostForm/PostFormModal.tsx";
+import {TopNav} from "../../../shared/components/TopNav.tsx";
 
 
 
@@ -8,30 +7,8 @@ import {PostFormModal} from "../../PostForm/PostFormModal.tsx";
 export function Home() {
     return (
         <>
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <Navbar.Brand href="#home">Dreamery</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                </Container>
-            </Navbar>
-
-
-            <Row>
-                <Col>
-                    <Container>
-                        <p>Placeholder for Find a Dream</p>
-                    </Container>
-                </Col>
-                <Col>
-                    <img src={'../images/Dreamery-Logo.png'} alt={'Logo'}/>
-                </Col>
-                <Col>
-                    <Container>
-                    </Container>
-                </Col>
-            </Row>
+            <TopNav />
             <PostFormModal/>
-            <DreamPost/>
 
         </>
     )
